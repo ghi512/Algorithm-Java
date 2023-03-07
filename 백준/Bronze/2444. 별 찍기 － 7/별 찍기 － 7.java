be9dec8335eb93a559f36f8aaddae595bce8cb46
김민지 <ghi512151@gmail.com>
2023-03-08 08:23:34 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 132 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -6,24 +6,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i=1; i<=n; i++) {
             for(int j=n-i; j>0; j--) {
-                System.out.print(' ');
+                sb.append(' ');
             }
             for(int j=1; j<=(i*2-1); j++) {
-                System.out.print('*');
+                sb.append('*');
             }
-            System.out.println();
+            sb.append('\n');
         }
 
         for(int i=n-1; i>0; i--) {
             for(int j=n-i; j>0; j--) {
-                System.out.print(' ');
+                sb.append(' ');
             }
             for(int j=(i*2-1); j>0; j--) {
-                System.out.print('*');
+                sb.append('*');
             }
-            System.out.println();
+            sb.append('\n');
         }
+
+        System.out.println(sb);
     }
 }

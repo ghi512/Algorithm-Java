542e4bb0665474e795ad0ba80232d86350fce298
김민지 <ghi512151@gmail.com>
2023-07-02 11:59:32 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.10 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,18 +1,6 @@
 class Solution {
     public String solution(String my_string, String letter) {
         String answer = "";
-        char ch = letter.charAt(0);
-        StringBuffer sb = new StringBuffer();
-        int strLen = my_string.length();
-        for(int i=0; i<strLen; i++) {
-            if(my_string.charAt(i) == ch) {
-                continue;
-            }
-            else {
-                sb.append(my_string.charAt(i));
-            }
-        }
-        answer = sb.toString();
         return answer;
     }
 }

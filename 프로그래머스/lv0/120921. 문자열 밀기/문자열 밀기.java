a18edcbd52219bae7440e2f2c47901614c84befc
김민지 <ghi512151@gmail.com>
2023-07-16 23:51:31 +0900

    [level 0] Title: 문자열 밀기, Time: 1.64 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(String A, String B) {
         int answer = 0;
-        String changedA = A;
-        for(int i=0; i<A.length(); i++) {
-            if(changedA.equals(B)) {
-                return answer;
-            }
-            String temp = changedA.substring(changedA.length()-1);
-            changedA = temp + changedA.substring(0, changedA.length()-1);
-            answer++;
-        }
-        
-        return -1;
+        return answer;
     }
 }
